7feb6060c70674c27e2d89d0b849a6f11abb14e5
Dario Freddi <drf@kde.org>
2009-11-04 01:43:42 +0100

    The new aqpmbuilder library for makepkg
    Signed-off-by: Dario Freddi <drf@kde.org>

---
+++ libaqpmbuilder/BuilderBackend.cpp
@@ -0,0 +1,303 @@
+/***************************************************************************
+ *   Copyright (C) 2009 by Dario Freddi                                    *
+ *   drf@chakra-project.org                                                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          *
+ ***************************************************************************/
+
+#include "BuilderBackend.h"
+
+#include <QFile>
+#include <qtextstream.h>
+#include <QPointer>
+#include <QProcess>
+#include <QTimer>
+#include <QDebug>
+
+namespace Aqpm {
+
+namespace Builder {
+
+class BuilderBackendHelper
+{
+public:
+    BuilderBackendHelper() : q(0) {}
+    ~BuilderBackendHelper() {
+        delete q;
+    }
+    Backend *q;
+};
+
+Q_GLOBAL_STATIC(BuilderBackendHelper, s_globalBuilderBackend)
+
+Backend *Backend::instance()
+{
+    if (!s_globalBuilderBackend()->q) {
+        new Backend;
+    }
+
+    return s_globalBuilderBackend()->q;
+}
+
+class Backend::Private
+{
+public:
+    Private() {}
+
+    QStringList fieldToStringList(const QString &pkgbuild, const QString &field) const;
+    QStringList optionsToStringList(QueueItem::Options options) const;
+
+    // Q_PRIVATE_SLOTS
+    void __k__itemFinished(int code, QProcess::ExitStatus eS);
+    void __k__processOutput();
+    void __k__processNextItem();
+
+    Backend *q;
+    QueueItem::List queue;
+    QueueItem::List::const_iterator queueIterator;
+    QPointer<QProcess> process;
+};
+
+QStringList Backend::Private::fieldToStringList(const QString& pkgbuild, const QString &field) const
+{
+    QFile fp(pkgbuild);
+    if (!fp.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        return QStringList();
+    }
+
+    QTextStream in(&fp);
+    QStringList retlist;
+
+    while (!in.atEnd()) {
+        QString line = in.readLine();
+
+        if (!line.startsWith(field)) {
+            continue;
+        }
+
+        QString testline(line);
+        testline.remove(' ');
+
+        while (true) {
+            if (line.contains('(')) {
+                line = line.split('(').at(1);
+            }
+
+            foreach(const QString &rdep, line.split(QChar('\''), QString::SkipEmptyParts)) {
+                QString dep(rdep);
+
+                if (!dep.contains(')') && !dep.contains(' ')) {
+                    if (dep.contains('>')) {
+                        dep.truncate(dep.indexOf('>'));
+                    }
+
+                    if (dep.contains('<')) {
+                        dep.truncate(dep.indexOf('<'));
+                    }
+
+                    if (dep.contains('=')) {
+                        dep.truncate(dep.indexOf('='));
+                    }
+
+                    qDebug() << "Adding" << dep;
+
+                    retlist.append(dep);
+                }
+            }
+
+            if (testline.endsWith(')')) {
+                break;
+            }
+
+            line = in.readLine();
+
+            testline = line;
+            testline.remove(' ');
+        }
+
+        break;
+    }
+
+    fp.close();
+}
+
+QStringList Backend::Private::optionsToStringList(QueueItem::Options options) const
+{
+    QStringList retlist;
+
+    if (options & QueueItem::Clean) {
+        retlist.append("--clean");
+    }
+    if (options & QueueItem::CleanCache) {
+        retlist.append("--cleancache");
+    }
+    if (options & QueueItem::HoldVersion) {
+        retlist.append("--holdver");
+    }
+    if (options & QueueItem::IgnoreArch) {
+        retlist.append("--ignorearch");
+    }
+    if (options & QueueItem::NoDeps) {
+        retlist.append("--nodeps");
+    }
+    if (options & QueueItem::SkipIntegrityChecks) {
+        retlist.append("--skipinteg");
+    }
+
+    return retlist;
+}
+
+void Backend::Private::__k__processNextItem()
+{
+    if (queueIterator == queue.constEnd()) {
+        // Queue finished! If we got here, it's a success
+        emit q->operationFinished(true);
+
+        // Cleanup
+        if (process) {
+            process->deleteLater();
+        }
+        queue.clear();
+
+        return;
+    }
+
+    if (process) {
+        process->deleteLater();
+    }
+
+    process = new QProcess();
+
+    process->setWorkingDirectory((*queueIterator).environmentPath);
+
+    connect(process, SIGNAL(readyReadStandardOutput()),
+            q, SLOT(__k__processOutput()));
+    /*connect(process, SIGNAL(readyReadStandardError()),
+            q, SLOT(__k__processOutput()));*/
+    connect(process, SIGNAL(finished(int, QProcess::ExitStatus)),
+            q, SLOT(__k__itemFinished(int, QProcess::ExitStatus)));
+
+    if (optionsToStringList((*queueIterator).options).isEmpty()) {
+        process->start("makepkg");
+    } else {
+        process->start("makepkg", optionsToStringList((*queueIterator).options));
+    }
+
+    emit q->buildEnvironmentStatusChanged((*queueIterator).environmentPath, Backend::Processing);
+}
+
+void Backend::Private::__k__processOutput()
+{
+    emit q->operationProgress(QString::fromLocal8Bit(process->readLine(1024)));
+}
+
+void Backend::Private::__k__itemFinished(int code, QProcess::ExitStatus /*eS*/)
+{
+    if (code != 0) {
+        // Item has failed. Clean it up
+        emit q->buildEnvironmentStatusChanged((*queueIterator).environmentPath, Backend::Failed);
+        emit q->operationFinished(false);
+
+        if (process) {
+            process->deleteLater();
+        }
+        queue.clear();
+
+        return;
+    } else {
+        // Success! Notify and move on
+        emit q->buildEnvironmentStatusChanged((*queueIterator).environmentPath, Backend::Succeeded);
+        ++queueIterator;
+        // Don't clutter the call stack, stack it into the event loop instead
+        QTimer::singleShot(0, q, SLOT(__k__processNextItem()));
+    }
+}
+
+Backend::Backend(QObject* parent)
+        : QObject(parent)
+        , d(new Private)
+{
+    Q_ASSERT(!s_globalBuilderBackend()->q);
+    s_globalBuilderBackend()->q = this;
+
+    d->q = this;
+}
+
+Backend::~Backend()
+{
+}
+
+bool Backend::addBuildEnvironmentToQueue(const QString& path, QueueItem::Options options) const
+{
+    if (!QFile::exists(path + "/PKGBUILD")) {
+        return false;
+    }
+
+    d->queue.append(QueueItem(path, options));
+    return true;
+}
+
+void Backend::clearQueue()
+{
+    d->queue.clear();
+}
+
+QueueItem::List Backend::queue() const
+{
+    return d->queue;
+}
+
+void Backend::processQueue()
+{
+    d->queueIterator = d->queue.constBegin();
+}
+
+QStringList Backend::dependsForQueue() const
+{
+    QStringList retlist;
+    foreach (const QueueItem &item, d->queue) {
+        retlist << d->fieldToStringList(item.environmentPath + "/PKGBUILD", "depends");
+    }
+    retlist.removeDuplicates();
+    return retlist;
+}
+
+QStringList Backend::makeDependsForQueue() const
+{
+    QStringList retlist;
+    foreach (const QueueItem &item, d->queue) {
+        retlist << d->fieldToStringList(item.environmentPath + "/PKGBUILD", "makedepends");
+    }
+    retlist.removeDuplicates();
+    return retlist;
+}
+
+QStringList Backend::allDependsForQueue() const
+{
+    QStringList retlist;
+    foreach (const QueueItem &item, d->queue) {
+        retlist << d->fieldToStringList(item.environmentPath + "/PKGBUILD", "depends");
+        retlist << d->fieldToStringList(item.environmentPath + "/PKGBUILD", "makedepends");
+    }
+    retlist.removeDuplicates();
+    return retlist;
+}
+
+}
+
+}
+
+#include "BuilderBackend.moc"
---
+++ libaqpmbuilder/BuilderBackend.h
@@ -0,0 +1,101 @@
+/***************************************************************************
+ *   Copyright (C) 2009 by Dario Freddi                                    *
+ *   drf@chakra-project.org                                                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          *
+ ***************************************************************************/
+
+#ifndef BUILDERBACKEND_H
+#define BUILDERBACKEND_H
+
+#include <QtCore/QObject>
+#include <QtCore/QStringList>
+
+namespace Aqpm {
+
+namespace Builder {
+
+class QueueItem
+{
+    public:
+        enum Option {
+            None = 0,
+            IgnoreArch = 1,
+            Clean = 2,
+            CleanCache = 4,
+            NoDeps = 8,
+            SkipIntegrityChecks = 16,
+            HoldVersion = 32
+        };
+        Q_DECLARE_FLAGS(Options, Option)
+
+        QueueItem();
+        QueueItem(const QString &path, Options options);
+
+        typedef QList<QueueItem> List;
+
+        QString environmentPath;
+        Options options;
+};
+
+class Backend : public QObject
+{
+    Q_OBJECT
+
+    public:
+        enum Status {
+            Queued = 0,
+            Processing = 1,
+            Succeeded = 2,
+            Failed = 3
+        };
+
+        static Backend *instance();
+
+        virtual ~Backend();
+
+        bool addBuildEnvironmentToQueue(const QString &path, QueueItem::Options options) const;
+        QueueItem::List queue() const;
+
+        QStringList makeDependsForQueue() const;
+        QStringList dependsForQueue() const;
+        QStringList allDependsForQueue() const;
+
+        void processQueue();
+        void clearQueue();
+
+    Q_SIGNALS:
+        void operationFinished(bool result);
+        void operationProgress(const QString &progress);
+        void buildEnvironmentStatusChanged(const QString &environment, Status status);
+
+    private:
+        Backend(QObject* parent = 0);
+
+        class Private;
+        Private * const d;
+
+        Q_PRIVATE_SLOT(d, void __k__itemFinished(int, QProcess::ExitStatus))
+        Q_PRIVATE_SLOT(d, void __k__processOutput())
+};
+
+}
+
+}
+
+Q_DECLARE_OPERATORS_FOR_FLAGS(Aqpm::Builder::QueueItem::Options)
+
+#endif // BUILDERBACKEND_H
